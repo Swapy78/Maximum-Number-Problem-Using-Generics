d4800c3a3c9a19a31d79a5d23034eb17b37bb460
Swapy78 <swapnilbabanmane@gmail.com>
2023-04-26 22:41:23 +0530

    Added Java program to find the maximum integer number

---
+++ src/demo/FindMaximumInteger.java
@@ -0,0 +1,52 @@
+//Java program to find the maximum integer number
+
+package demo;
+
+import java.util.Scanner;
+
+public class FindMaximumInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Creating a Scanner object to read user input from the console
+        Scanner sc = new Scanner(System.in);
+
+        // Telling the user to enter the first number
+        System.out.print("Enter the first number: ");
+
+        // Reading the first integer input from the user
+        Integer number1 = sc.nextInt();
+
+        // Prompt the user to enter the second number
+        System.out.print("Enter the second number: ");
+
+        // Reading the second integer input from the user
+        Integer number2 = sc.nextInt();
+
+        // Telling the user to enter the third number
+        System.out.print("Enter the third number: ");
+
+        // Reading the third integer input from the user
+        Integer number3 = sc.nextInt();
+
+        // Initializing the maximum number to be the first number
+        Integer maximum = number1;
+
+        // Checking if the second number is greater than the current maximum
+        if (number2.compareTo(maximum) > 0) {
+            maximum = number2;
+        }
+
+        // Checking if the third number is greater than the current maximum
+        if (number3.compareTo(maximum) > 0) {
+            maximum = number3;
+        }
+
+        // Printing the maximum number to the console
+        System.out.println("The maximum number is " + maximum);
+
+        // Closing the Scanner object to prevent memory leaks
+        sc.close();
+    }
+}
---
+++ src/demo/FindMaximumString.java
@@ -0,0 +1,53 @@
+//Java program to find the maximum string
+
+package demo;
+
+import java.util.Scanner;
+
+public class FindMaximumString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+        // Telling the user to enter the first string
+        System.out.print("Enter the first string: ");
+
+        // Reading the first string input from the user
+        String string1 = sc.nextLine();
+
+        // Telling the user to enter the second string
+        System.out.print("Enter the second string: ");
+
+        // Reading the second string input from the user
+        String string2 = sc.nextLine();
+
+        // Telling the user to enter the third string
+        System.out.print("Enter the third string: ");
+
+        // Reading the third string input from the user
+        String string3 = sc.nextLine();
+
+        // Initializing the maximum string to be the first string
+        String maximum = string1;
+
+        // Checking if the second string is greater than the current maximum
+        if (string2.compareTo(maximum) > 0) {
+            maximum = string2;
+        }
+
+        // Checking if the third string is greater than the current maximum
+        if (string3.compareTo(maximum) > 0) {
+            maximum = string3;
+        }
+
+        // Printing the maximum string to the console
+        System.out.println("The maximum string is " + maximum);
+
+        // Closing the Scanner object to prevent memory leaks
+        sc.close();
+    }
+}
+	
+
+
